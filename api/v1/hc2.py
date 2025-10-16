baa9ae67366c81f22713155020f052e4d924ec67
Akash Kumar <akash@getyourlawyers.com>
2025-10-16 17:48:31 +0530

    changes

--- api/v1/hc2.py
+++ api/v1/hc2.py
@@ -6,20 +6,17 @@ import re
 from pydantic import BaseModel
 from typing import Optional
 from dotenv import load_dotenv
-import pytesseract
-import cv2
-import random
-import json
-import numpy as np
 from core.database import collection
 from core.s3_client import s3_client
 import os
+import html
 load_dotenv()
 BUCKET_NAME = os.getenv("BUCKET_NAME")
 REGION_NAME = os.getenv("REGION_NAME")
 BASE_URL = "https://hcservices.ecourts.gov.in/hcservices/"
 
 app = APIRouter()
+
 
 class CaseRequest(BaseModel):
     case_type: str
@@ -29,21 +26,8 @@ class CaseRequest(BaseModel):
     dist_code: str
     court_complex_code: str
     est_code: Optional[str] = None
-    refresh_flag : str
+    refresh_flag: str
 
-
-def decode_captcha(session, captcha_url):
-    response = session.get(captcha_url, stream=True)
-
-    if response.status_code == 200:
-        image = np.asarray(bytearray(response.content), dtype=np.uint8)
-        image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
-
-        _, thresh_img = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY_INV)
-        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-        captcha_text = pytesseract.image_to_string(thresh_img, config="--psm 6").strip()
-        return captcha_text
-    return None
 
 def clean_text(text):
     return re.sub(r"\s+", " ", text.strip())
@@ -56,7 +40,8 @@ def extract_party_details(soup, class_name):
     for element in elements:
         for br in element.find_all("br"):
             br.replace_with("\n")
-        lines = [line.strip() for line in element.get_text().split("\n") if line.strip()]
+        lines = [line.strip()
+                 for line in element.get_text().split("\n") if line.strip()]
         if lines:
             name = lines[0]
 
@@ -70,7 +55,8 @@ def extract_party_details(soup, class_name):
 
 def extract_table_data(soup, heading, fields):
     extracted_data = {}
-    heading_element = soup.find("h2", string=re.compile(heading, re.IGNORECASE))
+    heading_element = soup.find(
+        "h2", string=re.compile(heading, re.IGNORECASE))
     if heading_element:
         table = heading_element.find_next("table")
         if table:
@@ -84,13 +70,15 @@ def extract_table_data(soup, heading, fields):
                         extracted_data[label] = value
     return extracted_data
 
+
 def extract_subordinate_court_info(soup):
     court_info = soup.find("span", class_="Lower_court_table")
     if not court_info:
         return {}
 
     details = {}
-    labels = court_info.find_all("span", style="width:150px;display:inline-block;")
+    labels = court_info.find_all(
+        "span", style="width:150px;display:inline-block;")
     values = court_info.find_all("label", style="text-align:left")
 
     for label, value in zip(labels, values):
@@ -100,13 +88,14 @@ def extract_subordinate_court_info(soup):
 
     return details
 
+
 def extract_high_court_case_history(soup):
     history = []
     for table in soup.find_all("table", {"class": "history_table"}):
         headers = [th.get_text(strip=True) for th in table.find_all("th")]
 
         if "Cause List Type" in headers and "Purpose of hearing" in headers:
-            rows = table.find_all("tr")[1:]  
+            rows = table.find_all("tr")[1:]
             for row in rows:
                 cols = row.find_all("td")
                 if len(cols) >= 5:
@@ -138,14 +127,14 @@ def extract_high_court_case_history(soup):
     return history
 
 
-def extract_and_upload_orders(soup, s3_client, session, BUCKET_NAME, REGION_NAME, case_info):
+def extract_and_upload_orders(soup, s3_client, session, BUCKET_NAME, REGION_NAME, value):
     orders = []
     table = soup.find("table", class_="order_table")
 
     if not table:
         return orders
 
-    rows = table.find_all("tr")[1:]  
+    rows = table.find_all("tr")[1:]
     for row in rows:
         cols = row.find_all("td")
         if len(cols) < 5:
@@ -160,8 +149,8 @@ def extract_and_upload_orders(soup, s3_client, session, BUCKET_NAME, REGION_NAME
 
         final_pdf_url = BASE_URL + link_tag["href"]
 
-        s3_folder_path = f"case_data/orders/{case_info['cino']}/"
-        s3_file_path = f"{s3_folder_path}{case_info['cino']}-{order_number}.pdf"
+        s3_folder_path = f"case_data/orders/{value}/"
+        s3_file_path = f"{s3_folder_path}{value}-{order_number}.pdf"
 
         try:
             s3_client.head_object(Bucket=BUCKET_NAME, Key=s3_file_path)
@@ -196,10 +185,11 @@ def extract_and_upload_orders(soup, s3_client, session, BUCKET_NAME, REGION_NAME
     return orders
 
 
-def parse_case_history(html, payload, second_payload, case_info, session):
+def parse_case_history(html, payload, second_payload, value, session):
     soup = BeautifulSoup(html, "html.parser")
 
-    case_details = extract_table_data(soup, "Case Details", ["Filing Number", "Filing Date", "Registration Number", "Registration Date", "CNR Number"])
+    case_details = extract_table_data(soup, "Case Details", [
+                                      "Filing Number", "Filing Date", "Registration Number", "Registration Date", "CNR Number"])
     case_status = extract_table_data(soup, "Case Status", [
         "First Hearing Date",
         "Decision Date",
@@ -214,9 +204,12 @@ def parse_case_history(html, payload, second_payload, case_info, session):
         "Stage of Case"
     ])
 
-    petitioner_and_advocate = extract_party_details(soup, "Petitioner_Advocate_table")
-    respondent_and_advocate = extract_party_details(soup, "Respondent_Advocate_table")
-    category_details = extract_table_data(soup, "Category", ["Category", "Sub Category"])
+    petitioner_and_advocate = extract_party_details(
+        soup, "Petitioner_Advocate_table")
+    respondent_and_advocate = extract_party_details(
+        soup, "Respondent_Advocate_table")
+    category_details = extract_table_data(
+        soup, "Category", ["Category", "Sub Category"])
     subordinate_court_info = extract_subordinate_court_info(soup)
     high_court_case_history = extract_high_court_case_history(soup)
     orders_hc = extract_and_upload_orders(
@@ -225,34 +218,35 @@ def parse_case_history(html, payload, second_payload, case_info, session):
         session=session,
         BUCKET_NAME=BUCKET_NAME,
         REGION_NAME=REGION_NAME,
-        case_info=case_info
+        value=value
     )
 
     return {
-            "case_no": second_payload.get("case_no"),
-            "case_reg_no": payload.get("case_no"),
-            "cino": second_payload.get("cino"),
-            "court_code": second_payload.get("court_code"),
-            "state_code": payload.get("state_code"),
-            "dist_code": payload.get("dist_code"),
-            "court_complex_code": payload.get("court_complex_code"),
-            "est_code": payload.get("est_code"),
-            "case_type": payload.get("case_type"),
-            "CaseType": payload.get("CaseType"),
-            "FilingNumber": case_details.get("Filing Number", ""),
-            "RegistrationNumber": case_details.get("Registration Number", ""),
-            "CNRNumber": second_payload.get("cino"),
-            "FirstHearingDate": case_status.get("First Hearing Date", ""),
-            "CaseStatus": case_status.get("Stage of Case", ""),
-            "CourtNumberandJudge": case_status.get("Coram", ""),
-            "petitioner_and_advocate": petitioner_and_advocate,
-            "respondent_and_advocate": respondent_and_advocate,
-            "actsandSection": {},
-            "category_details": category_details,
-            "subordinate_court_information": subordinate_court_info,
-            "case_history": high_court_case_history,
-            "case_transfer" : [],
-            "orders": orders_hc 
+        "case_no": second_payload.get("case_no"),
+        "case_reg_no": payload.get("case_no"),
+        "rgyear": payload.get("rgyear"),
+        "cino": second_payload.get("cino"),
+        "court_code": payload.get("court_code"),
+        "state_code": payload.get("state_code"),
+        "dist_code": payload.get("dist_code"),
+        "court_complex_code": payload.get("court_complex_code"),
+        "est_code": payload.get("est_code"),
+        "case_type": payload.get("case_type"),
+        "CaseType": payload.get("CaseType"),
+        "FilingNumber": case_details.get("Filing Number", ""),
+        "RegistrationNumber": case_details.get("Registration Number", ""),
+        "CNRNumber": second_payload.get("cino"),
+        "FirstHearingDate": case_status.get("First Hearing Date", ""),
+        "CaseStatus": case_status.get("Stage of Case", ""),
+        "CourtNumberandJudge": case_status.get("Coram", ""),
+        "petitioner_and_advocate": petitioner_and_advocate,
+        "respondent_and_advocate": respondent_and_advocate,
+        "actsandSection": {},
+        "category_details": category_details,
+        "subordinate_court_information": subordinate_court_info,
+        "case_history": high_court_case_history,
+        "case_transfer": [],
+        "orders": orders_hc
     }
 
 
@@ -260,6 +254,15 @@ def parse_case_history(html, payload, second_payload, case_info, session):
 def fetch_submit_hc_info(case_data: CaseRequest):
     session = requests.Session()
     query = case_data.dict()
+    ac_query = {
+        "case_reg_no": query.get("case_reg_no"),
+        "rgyear": query.get("rgyear"),
+        "est_code": query.get("est_code"),
+        "case_type": query.get("case_type"),
+        "state_code": query.get("state_code"),
+        "dist_code": query.get("dist_code"),
+        "court_complex_code": query.get("court_complex_code")
+    }
     if case_data.refresh_flag != "1":
         existing_case = collection.find_one(query)
         if existing_case:
@@ -268,79 +271,60 @@ def fetch_submit_hc_info(case_data: CaseRequest):
 
     try:
         payload = {
-            'court_code': case_data.court_complex_code,
-            'case_type': case_data.case_type,
-            'case_no': case_data.case_reg_no,
-            'rgyear': case_data.rgyear,
-            'state_code': case_data.state_code,
-            'dist_code': case_data.dist_code,
-            'caseStatusSearchType': 'CScaseNumber',
-            'court_complex_code': case_data.court_complex_code,
-            'est_code': case_data.est_code,
-            'caseNoType': 'new',
-            'search_case_no': case_data.case_reg_no
+            "action_code": "showRecords",
+            "state_code": case_data.state_code,
+            "dist_code": "1",
+            "case_type": case_data.case_type,
+            "case_no": case_data.case_reg_no,
+            "rgyear": case_data.rgyear,
+            "caseNoType": "new",
+            "displayOldCaseNo": "NO",
+            "captcha": "",
+            "court_code": case_data.court_complex_code,
         }
- #comment
 
-            captcha_url = f"https://hcservices.ecourts.gov.in/hcservices/securimage/securimage_show.php?{random.randint(100000,999999)}"
-            captcha_text = decode_captcha(session, captcha_url)
-            captcha_text = re.sub(r'[^A-Za-z0-9]', '', captcha_text)
-            payload['captcha'] = captcha_text
+        headers = {
+            'content-type': 'application/x-www-form-urlencoded',
+            'origin': 'https://hcservices.ecourts.gov.in',
+        }
+        response = session.post(
+            "https://hcservices.ecourts.gov.in/ecourtindiaHC/cases/case_no_qry.php", headers=headers, data=payload)
 
-            response = session.post(
-                "https://hcservices.ecourts.gov.in/hcservices/cases_qry/index_qry.php?action_code=showRecords",
-                data=payload
-            )
+        clean_text = response.text.lstrip('\ufeff')
+        clean_text = clean_text.replace("<br/>", " ")
+        decoded = html.unescape(html.unescape(clean_text))
+        values = decoded.split("~")
+        values[-1] = values[-1].replace("##", "")
+        print(values)
+        second_payload = {
+            "court_code": case_data.court_complex_code,
+            "state_code": case_data.state_code,
+            "court_complex_code": case_data.court_complex_code,
+            "case_no": values[0],
+            "cino": values[3],
+        }
+        headers = {
+            'Origin': 'https://hcservices.ecourts.gov.in',
+            'Referer': 'https://hcservices.ecourts.gov.in/',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        second_resp = session.post(
+            "https://hcservices.ecourts.gov.in/hcservices/cases_qry/o_civil_case_history.php",
+            data=second_payload,
+            headers=headers
+        )
 
-            raw_text = response.text.strip()
-            soup = BeautifulSoup(raw_text, "html.parser")
-            clean_text = soup.get_text()
+        testdata = parse_case_history(
+            second_resp.text, payload, second_payload, values[3], session)
+        result = collection.update_one(
+            ac_query, {"$set": testdata}, upsert=True)
+        if result.upserted_id:
+            testdata["_id"] = str(result.upserted_id)
+        else:
+            doc = collection.find_one(ac_query)
+            testdata["_id"] = str(doc["_id"])
 
-            tot_records_match = re.search(r'"totRecords"\s*:\s*(\d+)', clean_text)
-            tot_records = int(tot_records_match.group(1)) if tot_records_match else None
-
-            if tot_records == 0:
-                return JSONResponse(
-                    content={"error": "Invalid case details"},
-                    status_code=404
-                )
-
-
-            con_match = re.search(r'"con"\s*:\s*\["(.*?)"\]', clean_text)
-            case_details = None
-            if con_match:
-                con_data = con_match.group(1).encode('utf-8').decode('unicode_escape')
-                try:
-                    case_details = json.loads(con_data)[0]
-                except Exception:
-                    pass
-
-            if case_details:
-                second_payload = {
-                    "court_code": case_data.court_complex_code,
-                    "state_code": case_data.state_code,
-                    "court_complex_code": case_data.court_complex_code,
-                    "case_no": case_details.get("case_no"),
-                    "cino": case_details.get("cino"),
-                }
-
-                headers = {
-                'Origin': 'https://hcservices.ecourts.gov.in',
-                'Referer': 'https://hcservices.ecourts.gov.in/',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                }
-                second_resp = session.post(
-                    "https://hcservices.ecourts.gov.in/hcservices/cases_qry/o_civil_case_history.php",
-                    data=second_payload,
-                    headers=headers
-                )
-
-                testdata = parse_case_history(second_resp.text,payload,second_payload, case_details, session)
-
-                return testdata
-
-        
-        
+        return testdata
 
     finally:
         session.close()
