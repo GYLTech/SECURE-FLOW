69481d13424ec4b1f7d363e51d9f97e62d9e3d92
Akash Kumar <akash@getyourlawyers.com>
2026-01-17 14:37:33 +0530

    Sentry Addition

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+import sentry_sdk
 from api.v1 import districtcourt, hc2, cc, nclt,sci
 app = FastAPI(title="Secure Flow By GYL")
+
+sentry_sdk.init(
+    dsn="https://d5ba717dbd1a1f3eec57fb1ec6798284@o4508364047712256.ingest.us.sentry.io/4510724946853888",
+    send_default_pii=True,
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
+
 app.include_router(districtcourt.app, prefix="/api/v1", tags=["Districtcourt"])
-# app.include_router(hccourt.app, prefix="/api/v1", tags=["Hccourt"])
 app.include_router(hc2.app, prefix="/api/v1", tags=["Hccourt2"])
 app.include_router(cc.app, prefix="/api/v1", tags=["Consumer"])
 app.include_router(nclt.app, prefix="/api/v1", tags=["Nclt"])
