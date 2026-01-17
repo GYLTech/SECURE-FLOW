0108c14bbbc3ad5edc90ad1072df8cf6c7b656d3
Akash Kumar <akash@getyourlawyers.com>
2026-01-17 14:44:55 +0530

    changes

--- core/lambda_client.py
+++ core/lambda_client.py
@@ -6,5 +6,6 @@ load_dotenv()
 lambda_client = boto3.client(
     "lambda",
     aws_access_key_id=os.getenv("AWS_S3_KEY"),
-    aws_secret_access_key=os.getenv("AWS_S3SEC_KEY")
+    aws_secret_access_key=os.getenv("AWS_S3SEC_KEY"),
+    region_name="ap-south-1"
 )
